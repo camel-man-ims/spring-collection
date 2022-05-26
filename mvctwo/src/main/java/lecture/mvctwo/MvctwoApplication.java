6887ec781f02681ffe05c9f94eed5605c6c8055e
IMS <gudwnsrh@gmail.com>
2022-05-26 22:16:13 +0900

    22.05.26 mvc2 chap1 1/2

---
+++ mvctwo/src/main/java/lecture/mvctwo/MvctwoApplication.java
@@ -0,0 +1,12 @@
+package lecture.mvctwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvctwoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MvctwoApplication.class, args);
+	}
+
+}
