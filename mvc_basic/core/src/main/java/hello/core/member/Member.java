259bd9da55482833003d52ec127da5cfe66941ec
IMS <gudwnsrh@gmail.com>
2022-07-29 07:20:33 +0900

    22.07.29

---
+++ mvc_basic/core/src/main/java/hello/core/member/Member.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+}
